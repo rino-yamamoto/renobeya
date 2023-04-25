128f0e087a45983875875b29ec9ea89d043be1d3
r-yamamoto053 <r-yamamoto053@jutjoy.com>
2023-04-26 01:17:07 +0900

    html,css

--- js/6-1-8.js
+++ js/6-1-8.js
@@ -63,3 +63,21 @@
 	$(".js-menu").on("click", function () {
     	    $(this).next().slideToggle();
     	   });
+
+	// 最初の猫のスクロールで消える設定
+	var startPos = 0, winScrollTop = 0;
+// scrollイベントを設定
+window.addEventListener('scroll', function () {
+    winScrollTop = this.scrollY;
+    if (winScrollTop >= startPos) {
+        // 下にスクロールされた時
+        if (winScrollTop >= 200) {
+            // 下に200pxスクロールされたら隠す
+            document.getElementById('scrollArea').classList.add('hide');
+        }
+    } else {
+        // 上にスクロールされた時
+        document.getElementById('scrollArea').classList.remove('hide');
+    }
+    startPos = winScrollTop;
+});
