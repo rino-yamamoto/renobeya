2d408ef6cffb827d2759ea7dcb6fe40a32319dc3
r-yamamoto053 <r-yamamoto053@jutjoy.com>
2023-05-22 02:34:41 +0900

    css,js装

--- js/6-1-8.js
+++ js/6-1-8.js
@@ -137,38 +137,33 @@
 				});
 			}
 			
-			// 画面をスクロールをしたら動かしたい場合の記述
-			$(window).scroll(function (){
-				fadeAnime();/* アニメーション用の関数を呼ぶ*/
-			});// ここまで画面をスクロールをしたら動かしたい場合の記述
-			
-			// 画面が読み込まれたらすぐに動かしたい場合の記述
-			$(window).on('load', function(){
-				fadeAnime();/* アニメーション用の関数を呼ぶ*/
-			});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+		// 画面をスクロールをしたら動かしたい場合の記述
+		$(window).scroll(function (){
+			fadeAnime();/* アニメーション用の関数を呼ぶ*/
+		});// ここまで画面をスクロールをしたら動かしたい場合の記述
 		
+		// 画面が読み込まれたらすぐに動かしたい場合の記述
+		$(window).on('load', function(){
+			fadeAnime();/* アニメーション用の関数を呼ぶ*/
+		});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+	
 		}
 		};
 
-
+			
 		// りの部屋でできる3つのこと のスライド実装
-		$('.slider').slick({
-			autoplay: true,//自動的に動き出すか。初期値はfalse。
-			autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
-			speed:1000,//スライドの動きのスピード。初期値は300。
+		$('.inner_wrap3_slider').slick({
+			fade:false,//切り替えをフェードで行う。初期値はfalse。
+			autoplay: false,//自動的に動き出すか。初期値はfalse。
+			autoplaySpeed: 0,//次のスライドに切り替わる待ち時間
 			infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-			slidesToShow: 1,//スライドを画面に3枚見せる
-			slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-			arrows: true,//左右の矢印あり
-			prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-			nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-			dots: true,//下部ドットナビゲーションの表示
+			slidesToShow: 1,//スライドを画面に1枚見せる
+			slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+			arrows: true,//🌟左右の矢印あり
+			prevArrow: '<div class="slick-prev"></div>',//🌟矢印部分PreviewのHTMLを変更
+			nextArrow: '<div class="slick-next"></div>',//🌟矢印部分NextのHTMLを変更
+			dots: true,//🌟下部ドットナビゲーションの表示
 			pauseOnFocus: false,//フォーカスで一時停止を無効
 			pauseOnHover: false,//マウスホバーで一時停止を無効
 			pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
-		});
-	
-		//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
-		$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
-			$('.slider').slick('slickPlay');
 		});
